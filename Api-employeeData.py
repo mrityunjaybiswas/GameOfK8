cc9bb25844342d42f47ae3cfd0d6997f305d5587
mrityunjaybiswas <mrityunjay.biswas08@gmail.com>
2023-10-06 17:38:41 +0530

    v1.1 of GameOfk8

---
+++ Api-employeeData.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+location = "D:\PythonProjects\GameOfK8\empData.json"
+
+def get_data(location):
+    try:
+        with open(location, 'r') as fileData:
+            result = json.load(fileData)
+            print(result)
+        return result
+    except FileNotFoundError:
+        return None
+
+def get_employee_data(user_data, emp_id):
+    for user in user_data['users']:
+        if user.get('id') == emp_id:
+            return user
+    return None  
+
+@app.route('/userid', methods=['GET'])
+def getDetails():
+    user_id = request.args.get('id', type=int)
+
+    if user_id is None:
+        return jsonify({"error": "Please provide a valid 'id' parameter."}), 400
+    respData = get_data(location)
+    user_data = get_employee_data(respData, user_id)
+
+    if user_data:
+        return jsonify(user_data)
+    else:
+        return jsonify({"error": "User not found."}), 404
+
+if __name__ == "__main__":
+    app.run(debug=True,host=('0.0.0.0'))
